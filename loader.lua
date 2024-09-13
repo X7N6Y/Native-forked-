579d22ce69574a2adccbc34871e90f56bfe6ce4c
Native-lab <creations.of.native@gmail.com>
2024-09-14 00:08:25 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,28 @@
+local Projects = {
+	["Death Ball"] = {
+		GameId = 5166944221;
+		PlaceIds = {};
+		Loader = "https://api.luarmor.net/files/v3/loaders/c1e22c6997f84329ce2f8667f71828f8.lua";
+	};
+	["Anime Vanguards"] = {
+		GameId = 5578556129;
+		PlaceIds = {};
+		Loader = "https://api.luarmor.net/files/v3/loaders/c296c70e07b1a42087ce52d1d2625af6.lua";
+	};
+};
 
+local Loaded = false
+
+for i, v in pairs(Projects) do
+	if v.GameId == game.GameId then
+		Loaded = true
+
+		(loadstring or load)(game:HttpGet(v.Loader))()
+
+		break
+	end
+end
+
+if not Loaded then
+	setclipboard(tostring(game.GameId))
+end
